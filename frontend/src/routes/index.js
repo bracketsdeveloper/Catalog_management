3396cdc2970fefebe9b53ded8b7b5dfa8b6e6c54
ABHISHEK KUMAR <abhishekblog6315@gmail.com>
2025-05-04 11:56:23 +0530

    index.js frontend

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -54,6 +54,8 @@ import SamplesOut from "../pages/SamplesOut";
 import SampleStatus from "../pages/SampleStatus";
 import ManageExpenses from "../pages/ManageExpenses";
 
+import { ManageVendors } from "../components/manageVendors/manageVendors";
+
 import ManagePotentialClients from "../pages/ManagePotentialClients";
 import EventManager from "../pages/EventManager";
 import PotentialClientsList from "../pages/PotentialClientsList";
